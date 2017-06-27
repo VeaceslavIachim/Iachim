0207dee28b6dfd367ca3ea02eff999b9e15ec279
Veaceslav Iachim <veaceslav.iachim@amdaris.com>
2017-06-27 16:27:57 +0300

    First

---
+++ test/test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Marian";
+
+            Console.WriteLine("Buna ziua");
+            Console.WriteLine("salut");
+            Console.WriteLine("Commit to branch");
+            Console.WriteLine("Yout mentor's name is {0}", name);
+            Console.WriteLine("Eu sunt pe loc");
+            Console.WriteLine("Deja nu mai sunt");
+
+        }   
+    }
+}
